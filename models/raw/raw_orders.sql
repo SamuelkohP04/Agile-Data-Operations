295da9b853b0c4fe72053b101aad301446af055d
Samuel Koh <s10243052@connect.np.edu.sg>
2024-01-07 11:49:37 +0000

    This is for the rest of the code that i forgot to commit and sync, as I could have saved everything and committed everything in one go

--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -1,22 +1,4 @@
-UPDATE raw_orders
-SET ordersellingprice = ABS(ordersellingprice)
-WHERE orderid IN (
-    SELECT orderid
-    FROM (
-        SELECT orderid, SUM(ordersellingprice) AS total_sp
-        FROM raw_orders
-        GROUP BY orderid
-        HAVING total_sp < 0
-    ) AS subquery
-);
-with
-orders as (
-    select * from raw_orders
-)
+{{ config (materialized='table')}}
 
-select
-    orderid,
-    sum(ordersellingprice) as total_sp
-from orders
-group by orderid
-having total_sp < 0
+select *
+from {{ source('PRICESMART', 'ORDERS') }}
