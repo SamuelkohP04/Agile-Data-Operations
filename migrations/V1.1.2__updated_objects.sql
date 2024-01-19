ed48364427f052a47451d81d039282a9e244ee82
Sam <samuelkoh17@gmail.com>
2024-01-19 22:40:47 +0800

    tried the DECIMAL again

--- migrations/V1.1.2__updated_objects.sql
+++ migrations/V1.1.2__updated_objects.sql
@@ -6,7 +6,8 @@ USE SCHEMA PRICESMART;
 -- Log before ALTER TABLE statement
 --PRINT 'Adding column DISCOUNT to table ORDERS';
 
-ALTER TABLE ORDERS ADD COLUMN DISCOUNT(5,2);
+ALTER TABLE ORDERS ADD COLUMN DISCOUNT DECIMAL(5,2);
+
 -- precision of 5, and a scale of 2
 -- decimal function allows a specific data type and precision
 
