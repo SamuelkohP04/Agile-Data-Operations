8431b09e42384371d094200d420dedb7c06fb29e
Sam <samuelkoh17@gmail.com>
2024-01-19 23:58:49 +0800

    Changed back to original ALTER TABLE ORDERS ADD COLUMN DISCOUNT(5,2);

--- migrations/V1.1.2__updated_objects.sql
+++ migrations/V1.1.2__updated_objects.sql
@@ -6,13 +6,6 @@ USE SCHEMA PRICESMART;
 -- Log before CREATE OR REPLACE TABLE statement
 --PRINT 'Creating or replacing table ORDERS';
 
-BEGIN
-  ALTER TABLE ORDERS
-  ADD COLUMN IF NOT EXISTS DISCOUNT DECIMAL(5,2);
-EXCEPTION
-  WHEN OTHERS THEN
-    NULL; -- Do nothing if the column already exists
-END;
-
+ALTER TABLE ORDERS ADD COLUMN DISCOUNT(5,2);
 -- Log script execution end
 --PRINT 'Script execution completed successfully';
