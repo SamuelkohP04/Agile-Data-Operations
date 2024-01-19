fe58f6b1af977c6c39d580f491ca7fa2dc0574fb
Sam <samuelkoh17@gmail.com>
2024-01-19 22:36:03 +0800

    Removed the debug print statements

--- migrations/V1.1.2__updated_objects.sql
+++ migrations/V1.1.2__updated_objects.sql
@@ -1,15 +1,15 @@
 -- Log script execution start
-PRINT 'Executing script V1.1.2__updated_objects.sql';
+--PRINT 'Executing script V1.1.2__updated_objects.sql';
 
 USE SCHEMA PRICESMART;
 
 -- Log before ALTER TABLE statement
-PRINT 'Adding column DISCOUNT to table ORDERS';
+--PRINT 'Adding column DISCOUNT to table ORDERS';
 
 ALTER TABLE ORDERS ADD COLUMN DISCOUNT(5,2);
 -- precision of 5, and a scale of 2
 -- decimal function allows a specific data type and precision
 
 -- Log script execution end
-PRINT 'Script execution completed successfully';
+--PRINT 'Script execution completed successfully';
 -- pushing again for auto workflow
