98ed1b04abec318dbc93d2786d38a8ba7d8f3de0
Sam <samuelkoh17@gmail.com>
2024-01-19 21:44:06 +0800

    Simplified the statement for ALTER TABLE to its basic form

--- models/raw/migrations/V1.1.2__updated_objects.sql
+++ models/raw/migrations/V1.1.2__updated_objects.sql
@@ -6,7 +6,7 @@ USE SCHEMA PRICESMART;
 -- Log before ALTER TABLE statement
 PRINT 'Adding column DISCOUNT to table ORDERS';
 
-ALTER TABLE ORDERS ADD COLUMN DISCOUNT DECIMAL(5,2) DEFAULT 0;
+ALTER TABLE ORDERS ADD COLUMN DISCOUNT DECIMAL;
 -- precision of 5, and a scale of 2
 -- decimal function allows a specific data type and precision
 
