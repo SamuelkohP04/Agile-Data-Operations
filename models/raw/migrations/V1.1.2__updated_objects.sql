189e03e85b0b026e40fec0d31887a63fd8703b7d
Sam <samuelkoh17@gmail.com>
2024-01-19 20:56:17 +0800

    Troubleshooting why workflow has a syntax error, so i added DECIMAL keyword with a specific data type

--- models/raw/migrations/V1.1.2__updated_objects.sql
+++ models/raw/migrations/V1.1.2__updated_objects.sql
@@ -1,3 +1,4 @@
 USE SCHEMA PRICESMART;
-ALTER TABLE ORDERS ADD COLUMN DISCOUNT(5,2);
+ALTER TABLE ORDERS ADD COLUMN DISCOUNT DECIMAL(5,2);
 -- precision of 5, and a scale of 2
+-- decimal function allows a specific data type and precision
